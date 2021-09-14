d7929c17965281b33656bb40aa80622f92669a35
Thainnt <thai164@gmail.com>
2021-09-14 21:26:47 +0000

    First add assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+//FUNCTION IMPLEMENTATION
+const eqArrays = function(ar1, ar2) {
+  if (ar1.length === ar2.length) {
+    for (let i = 0; i < ar1.length; i++) {
+      if(ar1[i] !== ar2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//TEST CODE
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => should PASS
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
+
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));
+
+console.log(assertArraysEqual([], [])); //test empty array
+console.log(assertArraysEqual(["1", "2", ["3"]], ["1", "2", ["3"]])); // test nested arrays
