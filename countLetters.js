e6aa892dd6347845a5af8487190b625e8702dc99
Thainnt <thai164@gmail.com>
2021-09-15 21:24:34 +0000

    First add countLetter.js

---
+++ countLetters.js
@@ -0,0 +1,37 @@
+//IMPLEMENTATION CODE
+const countLetters = function (str) {
+  const countString = str.split(" ").join("").toLowerCase();
+  let countResults = {};
+  for (let char of countString) {
+    if(countResults[char]) {
+      countResults[char] += 1;
+    } else {
+      countResults[char] = 1;
+    }
+  }
+  return countResults;
+};
+
+
+//TEST FUNCTION
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💯💯💯Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👻👻👻Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//TEST CODE
+const sentence = "lighthouse in the house";
+console.log(assertEqual(countLetters(sentence)["l"], 1));
+console.log(assertEqual(countLetters(sentence)["i"], 2));
+console.log(assertEqual(countLetters(sentence)["g"], 1));
+console.log(assertEqual(countLetters(sentence)["h"], 4));
+console.log(assertEqual(countLetters(sentence)["t"], 2));
+console.log(assertEqual(countLetters(sentence)["o"], 2));
+console.log(assertEqual(countLetters(sentence)["u"], 2));
+console.log(assertEqual(countLetters(sentence)["s"], 2));
+console.log(assertEqual(countLetters(sentence)["e"], 3));
+console.log(assertEqual(countLetters(sentence)["n"], 1));
