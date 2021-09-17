1b143f75742b858cec1663fcc359d8597cf352e7
Thainnt <thai164@gmail.com>
2021-09-17 23:44:17 +0000

    Commit revised eqArrays.js with recursive function to compare nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,32 +1,54 @@
 //FUNCTION IMPLEMENTATION
 const eqArrays = function(ar1, ar2) {
+  let result;
   if (ar1.length === ar2.length) {
+    // BOTH ARRAYS ARE EMPTY
+    if (ar1.length === 0) {
+      return result = true;
+    }
+    //ARRAYS ARE NOT EMPTY
     for (let i = 0; i < ar1.length; i++) {
-      if(ar1[i] !== ar2[i]) {
-        return false;
+      if(ar1[i] === ar2[i]) { // BASE CASE: ONE LEVEL ARRAY
+        result = true;
+      } else {
+        if (Array.isArray(ar1[i]) && Array.isArray(ar2[i])) { //CHECK IF THERE IS ANOTHER LEVEL OF ARRAY
+          if (eqArrays(ar1[i], ar2[i])) {
+            result = true;
+          } else {
+            return result = false;
+          }
+        } else {
+          return result = false;
+        }
       }
     }
-    return true;
   } else {
     return false;
   }
+
+  return result;
 }
+
+// base all item of arrays are equal
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`💯💯💯Assertion Passed: ${actual} === ${expected}`);
+    console.log(`💯Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`👻👻👻Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`👻Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 //TEST CODE
+console.log("One level arrays:")
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
-
-assertEqual(eqArrays([], []), true); //test empty array
-assertEqual(eqArrays(["1", "2", ["3"]], ["1", "2", ["3"]]), true); // test nested arrays
-assertEqual(eqArrays([{type: "car", color: "black"}, {type: "bike", color: "green"}], [{type: "car", color: "black"}, {type: "bike", color: "green"}]), true); //test array of objects
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+console.log("Empty arrays:")
+assertEqual(eqArrays([], []), true);
+console.log("Nested arrays:")
+assertEqual(eqArrays(["1", "2", ["3"]], ["1", "2", ["3"]]), true);
+assertEqual(eqArrays([[0, 4, 9, [8, "a", "o"]], "2", ["3", 7, "10"]], [[0, 4, 9, [8, "a", "o"]], "2", ["3", 7, "10"]]), true);
+assertEqual(eqArrays([[0, 4, 9, [8, "a", "o"]], "2", "8", ["3", 7, "10"]], [[0, 4, 9, [8, "a", "o"]], "2", ["3", 7, "10"]]), false);
+// assertEqual(eqArrays([{type: "car", color: "black"}, {type: "bike", color: "green"}], [{type: "car", color: "black"}, {type: "bike", color: "green"}]), true); //test array of objects
