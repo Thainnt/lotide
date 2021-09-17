8d08d9e9e3843319fc653837743c66fc7d400c36
Thainnt <thai164@gmail.com>
2021-09-17 22:41:17 +0000

    Commit revised eqObject.js comparing Objects using recursive function

--- eqObjects.js
+++ eqObjects.js
@@ -28,8 +28,10 @@ const eqObjects = function(object1, object2) {
         if (!eqArrays(object1[key], object2[key])) {
           return result = false;
         }
+      } else if (object1[key] === object2[key]) {
+        result = true;
       } else {
-        if (object1[key] !== object2[key]) {
+        if (!eqObjects(object1[key], object2[key])/*object1[key] !== object2[key]*/) {
           return result = false;
         } else {
           result = true;
@@ -44,9 +46,9 @@ const eqObjects = function(object1, object2) {
 //TEST FUNCTION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`💯💯💯Assertion Passed: ${actual} === ${expected}`);
+    console.log(`😍Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`👻👻👻Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`😱Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 //TEST CODE
@@ -64,3 +66,10 @@ console.log(assertEqual(eqObjects(cd, dc), true));
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 console.log(assertEqual(eqObjects(cd, cd2), false));
+//OBJECTS
+console.log(assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true));
+console.log(assertEqual(eqObjects({ a: { z: {i: 3, j : {n: 0}} }, b: 2 }, { a: { z: {i: 3, j : {n: 0}} }, b: 2 }), true));
+console.log(assertEqual(eqObjects({ a: { z: {i: [9, 8, 7], j : {n: 0}} }, b: 2 }, { a: { z: {i: [9, 8, 7], j : {n: 0}} }, b: 2 }), true));
+console.log(assertEqual(eqObjects({ a: { z: {i: 3, j : {n: 0}} }, b: 2 }, { a: { z: {i: 3, j : {n: [1, 2]}} }, b: 2 }), false));
+console.log(assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false));
+console.log(assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false)); // => false
