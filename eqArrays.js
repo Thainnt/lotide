d39837e851f34eafe153e4efc467ca06f4413a4e
Thainnt <thai164@gmail.com>
2021-09-20 17:41:43 +0000

    commit eqArrays.js and test file

--- eqArrays.js
+++ eqArrays.js
@@ -28,27 +28,4 @@ const eqArrays = function(ar1, ar2) {
 
   return result;
 }
-
-// base all item of arrays are equal
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💯Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👻Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//TEST CODE
-console.log("One level arrays:")
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-console.log("Empty arrays:")
-assertEqual(eqArrays([], []), true);
-console.log("Nested arrays:")
-assertEqual(eqArrays(["1", "2", ["3"]], ["1", "2", ["3"]]), true);
-assertEqual(eqArrays([[0, 4, 9, [8, "a", "o"]], "2", ["3", 7, "10"]], [[0, 4, 9, [8, "a", "o"]], "2", ["3", 7, "10"]]), true);
-assertEqual(eqArrays([[0, 4, 9, [8, "a", "o"]], "2", "8", ["3", 7, "10"]], [[0, 4, 9, [8, "a", "o"]], "2", ["3", 7, "10"]]), false);
-// assertEqual(eqArrays([{type: "car", color: "black"}, {type: "bike", color: "green"}], [{type: "car", color: "black"}, {type: "bike", color: "green"}]), true); //test array of objects
+module.exports = eqArrays;
